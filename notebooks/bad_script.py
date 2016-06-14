007e460cee8b94bfc4d673f7bb3c2655415094fe
Gil Forsyth <gilforsyth@gmail.com>
2016-06-14 17:14:05 -0400

    add intro to profiling notebook + demo script
    still need to call out timeout somewhere...

---
+++ notebooks/bad_script.py
@@ -0,0 +1,26 @@
+import numpy
+from time import sleep
+
+def bad_call(dude):
+    sleep(1)
+    
+def worse_call(dude):
+    sleep(2)
+    
+def sumulate(foo):
+    if not isinstance(foo, int):
+        return
+   
+    a = numpy.random.random((1000, 1000))
+    a @ a
+    ans = 0
+    for i in range(foo):
+        ans += i
+        
+    bad_call(ans)
+    worse_call(ans)
+    
+    return ans
+
+if __name__ == '__main__':
+    sumulate(15)
