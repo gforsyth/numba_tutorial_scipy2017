be8f84cbd066d8cb964bad9e79077f59bcd67e28
Gil Forsyth <gilforsyth@gmail.com>
2016-06-20 12:41:26 -0400

    change ppe solve from j, i -> i, j

--- notebooks/snippets/ns_helper.py
+++ notebooks/snippets/ns_helper.py
@@ -22,8 +22,8 @@ def cavity_flow(u, v, p, nt, dt, dx, velocity_term, pressure_poisson, rho=1, nu=
 
     un = numpy.empty_like(u)
     vn = numpy.empty_like(v)
-    ny, nx = u.shape
-    b = numpy.zeros((ny, nx))
+    nx, ny = u.shape
+    b = numpy.zeros((nx, ny))
 
     for n in range(nt):
         un = u.copy()
@@ -34,40 +34,40 @@ def cavity_flow(u, v, p, nt, dt, dx, velocity_term, pressure_poisson, rho=1, nu=
 
         u[1:-1,1:-1] = (un[1:-1, 1:-1] - dt / dx *
                         (un[1:-1,1:-1] * (un[1:-1, 1:-1] -
-                                          un[1:-1, :-2]) +
-                        vn[1:-1, 1:-1 ] * (un[1:-1, 1:-1] -
-                                            un[:-2, 1:-1]) +
-                        1 / (2 * rho) * (p[1:-1, 2:] -
-                                         p[1:-1, :-2])) +
+                                          un[:-2, 1:-1]) +
+                        vn[1:-1, 1:-1] * (un[1:-1, 1:-1] -
+                                            un[1:-1, :-2]) +
+                        1 / (2 * rho) * (p[2:, 1:-1] -
+                                         p[:-2, 1:-1])) +
                           nu * dt / dx**2 *
-                         (un[1:-1, 2:] +
-                          un[1:-1, :-2] +
-                          un[2:, 1:-1] +
-                          un[:-2, 1:-1] -
+                         (un[2:, 1:-1] +
+                          un[:-2, 1:-1] +
+                          un[1:-1, 2:] +
+                          un[1:-1, :-2] -
                           4 * un[1:-1, 1:-1]))
 
         v[1:-1,1:-1] = (vn[1:-1, 1:-1] - dt / dx *
-                        (un[1:-1,1:-1] * (vn[1:-1, 1:-1] -
-                                          vn[1:-1, :-2]) +
+                        (un[1:-1, 1:-1] * (vn[1:-1, 1:-1] -
+                                          vn[:-2, 1:-1]) +
                          vn[1:-1, 1:-1 ] * (vn[1:-1, 1:-1] -
-                                            vn[:-2, 1:-1]) +
-                        1 / (2 * rho) * (p[2:, 1:-1] -
-                                         p[:-2, 1:-1])) +
+                                            vn[1:-1, :-2]) +
+                        1 / (2 * rho) * (p[1:-1, 2:] -
+                                         p[1:-1, :-2])) +
                         nu * dt / dx**2 *
-                         (vn[1:-1, 2:] +
-                          vn[1:-1, :-2] +
-                          vn[2:, 1:-1] +
-                          vn[:-2, 1:-1] -
+                         (vn[2:, 1:-1] +
+                          vn[:-2, 1:-1] +
+                          vn[1:-1, 2:] +
+                          vn[1:-1, :-2] -
                           4 * vn[1:-1, 1:-1]))
 
-        u[:, 0] = 0
-        u[:, -1] = 0
-        v[:, 0] = 0
-        v[:, -1] = 0
         u[0, :] = 0
-        u[-1, :] = 1    #set velocity on cavity lid equal to 1
+        u[-1, :] = 0
         v[0, :] = 0
         v[-1, :] = 0
+        u[:, 0] = 0
+        u[:, -1] = 1    #set velocity on cavity lid equal to 1
+        v[:, 0] = 0
+        v[:, -1] = 0
 
     return u, v, p
 
@@ -77,7 +77,7 @@ def quiver_plot(u, v, p, nx=41):
     ny = nx
     x = numpy.linspace(0, 2, nx)
     y = numpy.linspace(0, 2, ny)
-    X, Y = numpy.meshgrid(x, y)
+    X, Y = numpy.meshgrid(x, y, indexing='ij')
 
     quiver_skip = qs = 4
     pyplot.figure(figsize=(11, 7), dpi=100)
@@ -92,14 +92,14 @@ def quiver_plot(u, v, p, nx=41):
 def velocity_term(b, rho, dt, u, v, dx):
     b[1:-1, 1:-1] = (
         rho * dx / 16 *
-        (2 / dt * (u[1:-1, 2:] -
-                   u[1:-1, :-2] +
-                   v[2:, 1:-1] -
-                   v[:-2, 1:-1]) -
-         2 / dx * (u[2:, 1:-1] - u[:-2, 1:-1]) *
-                  (v[1:-1, 2:] - v[1:-1, :-2]) -
-        (u[1:-1, 2:] - u[1:-1, :-2])**2 / dx -
-        (v[2:, 1:-1] - v[:-2, 1:-1])**2 / dx)
-                    )
+        (2 / dt * (u[2:, 1:-1] -
+                    u[:-2, 1:-1] +
+                    v[1:-1, 2:] -
+                    v[1:-1, :-2]) -
+        2 / dx * (u[1:-1, 2:] - u[1:-1, :-2]) *
+                 (v[2:, 1:-1] - v[:-2, 1:-1]) -
+        (u[2:, 1:-1] - u[:-2, 1:-1])**2 / dx -
+        (v[1:-1, 2:] - v[1:-1, :-2])**2 / dx)
+                     )
 
     return b
--- notebooks/snippets/ppe_numba.py
+++ notebooks/snippets/ppe_numba.py
@@ -1,33 +1,31 @@
 @jit(nopython=True)
 def pressure_poisson(p, b, l2_target=1e-3):
     pn = p.copy()
-    J, I = b.shape
+    I, J = b.shape
 
     iter_diff = l2_target + 1
 
     n = 0
-    while iter_diff > l2_target:
+    while iter_diff > l2_target and n <= 500:
         pn = p.copy()
-        for j in range(1, J - 1):
-            for i in range(1, I - 1):
-                p[j, i] = (.25 * (pn[j, i + 1] +
-                                  pn[j, i - 1] +
-                                  pn[j + 1, i] +
-                                  pn[j - 1, i]) -
-                                  b[j, i])
-
-        for j in range(J):
-            p[j, 0] = p[j, 1]
-            p[j, -1] = p[j, -2]
+        for i in range(1, I - 1):
+            for j in range(1, J - 1):
+                p[i, j] = (.25 * (pn[i, j + 1] +
+                                  pn[i, j - 1] +
+                                  pn[i + 1, j] +
+                                  pn[i - 1, j]) -
+                                  b[i, j])
 
         for i in range(I):
-            p[0, i] = p[1, i]
-            p[-1, i] = 0
+            p[i, 0] = p[i, 1]
+            p[i, -1] = p[i, -2]
+
+        for j in range(J):
+            p[0, j] = p[1, j]
+            p[-1, j] = 0
 
         if n % 10 == 0:
             iter_diff = numpy.sqrt(numpy.sum((p - pn)**2)/numpy.sum(pn**2))
-        if n == 500:
-            break
 
         n += 1
 
