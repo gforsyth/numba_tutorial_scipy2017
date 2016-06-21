9feeed7070a8e56179abfb84981d2bae152cb4a9
Gil Forsyth <gilforsyth@gmail.com>
2016-06-21 16:00:46 -0400

    update edge conditions in interpolation

--- notebooks/snippets/guvectorize/interpolation.py
+++ notebooks/snippets/guvectorize/interpolation.py
@@ -3,8 +3,8 @@
               '(float64[:,:], float64[:], float64[:,:])'], '(n,n),(m)->(m,m)')
 def interpolate_2d_gvec(coarse, size, fine):
     I, J = coarse.shape
-    for i in range(1, I - 1):
-        for j in range(1, J - 1):
+    for i in range(0, I - 1):
+        for j in range(0, J - 1):
             fine[2 * i, 2 * j] = coarse[i, j]
             fine[2 * i + 1, 2 * j] = .5 * (coarse[i, j] +
                                             coarse[i + 1, j])
@@ -14,3 +14,11 @@ def interpolate_2d_gvec(coarse, size, fine):
                                                 coarse[i + 1, j] +
                                                 coarse[i, j + 1] +
                                                 coarse[i + 1, j + 1])
+
+        fine[2 * i, 2 * (J - 1)] = coarse[i, (J - 1)]
+        fine[2 * i + 1, 2 * (J - 1)] = .5 * (coarse[i, J - 1] +
+                                            coarse[i + 1, J - 1])
+    for j in range(0, J):
+        fine[2 * (I - 1), 2 * j] = coarse[(I - 1), j]
+        fine[2 * (I - 1), 2 * j + 1] = .5 * (coarse[I - 1, j] +
+                                    coarse[I - 1, j + 1])
