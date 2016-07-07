b8b105c0bd1d13da7e3133a0a981a7dfe2745747
Gil Forsyth <gilforsyth@gmail.com>
2016-07-07 16:14:52 -0400

    language tweaks and minor fixes

--- notebooks/snippets/nbody/create_n.py
+++ notebooks/snippets/nbody/create_n.py
@@ -6,10 +6,10 @@ def create_n_random_particles(n, m, domain=1):
     '''
     parts = numpy.zeros((n), dtype=particle_dtype)
     #attribute access only in @jitted function
-    for i in parts:
-        i.x = numpy.random.random() * domain 
-        i.y = numpy.random.random() * domain
-        i.z = numpy.random.random() * domain
-        i.m = m
-        i.phi = 0
+    for p in parts:
+        p.x = numpy.random.random() * domain
+        p.y = numpy.random.random() * domain
+        p.z = numpy.random.random() * domain
+        p.m = m
+        p.phi = 0
     return parts
